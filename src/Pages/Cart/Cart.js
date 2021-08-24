d33761a15caf69ec689fcc5a1dd675c561eb776e
Aavar Khatiwoda <aavarkhatiwoda@gmail.com>
2021-08-23 21:23:02 -0400

    add fontsize1 to cart text

--- src/Pages/Cart/Cart.js
+++ src/Pages/Cart/Cart.js
@@ -12,28 +12,30 @@ function CartItems() {
           {esi.map(i => (
             <div className={"cart_item" + i.itemnumber} id={"cart_item" + i.itemnumber}>
               <div className="cart_cards_contents">
+                <div className="fontsize1">
 
 
-                <img className="cart_item_image" id={"cart_item" + i.itemnumber + "_image"} />
-                <div className="cart_item_name" id={"cart_item" + i.itemnumber + "_name"} />
-                <div className="cart_item_quantity" id={"cart_item" + i.itemnumber + "_quantity"} />
-                <div className="cart_item_add1" id={"cart_item" + i.itemnumber + "_add1"} onClick={function() {
-                  i.quantity += 1;
-                  document.getElementById('cart_item' + i.itemnumber + '_quantity').innerHTML = "$" + i.price + "\xa0\xa0·\xa0\xa0x" + i.quantity + "\xa0\xa0·\xa0\xa0" + "$" + i.price*i.quantity;
-                }}>{"ADD\xa0\xa0"}</div><span style={{color:'#646464'}}>{"·\xa0\xa0"}</span>
-                <div className="cart_item_subtract1" id={"cart_item" + i.itemnumber + "_subtract1"} onClick={function() {
-                  i.quantity -= 1;
-                  document.getElementById('cart_item' + i.itemnumber + '_quantity').innerHTML = "$" + i.price + "\xa0\xa0·\xa0\xa0x" + i.quantity + "\xa0\xa0·\xa0\xa0" + "$" + i.price*i.quantity;
-                  if (i.quantity == 0) {
+                  <img className="cart_item_image" id={"cart_item" + i.itemnumber + "_image"} />
+                  <div className="cart_item_name" id={"cart_item" + i.itemnumber + "_name"} />
+                  <div className="cart_item_quantity" id={"cart_item" + i.itemnumber + "_quantity"} />
+                  <div className="cart_item_add1" id={"cart_item" + i.itemnumber + "_add1"} onClick={function() {
+                    i.quantity += 1;
+                    document.getElementById('cart_item' + i.itemnumber + '_quantity').innerHTML = "$" + i.price + "\xa0\xa0·\xa0\xa0x" + i.quantity + "\xa0\xa0·\xa0\xa0" + "$" + i.price*i.quantity;
+                  }}>{"ADD\xa0\xa0"}</div><span style={{color:'#646464'}}>{"·\xa0\xa0"}</span>
+                  <div className="cart_item_subtract1" id={"cart_item" + i.itemnumber + "_subtract1"} onClick={function() {
+                    i.quantity -= 1;
+                    document.getElementById('cart_item' + i.itemnumber + '_quantity').innerHTML = "$" + i.price + "\xa0\xa0·\xa0\xa0x" + i.quantity + "\xa0\xa0·\xa0\xa0" + "$" + i.price*i.quantity;
+                    if (i.quantity == 0) {
+                      document.getElementById('cart_item' + i.itemnumber).style.display = "none";
+                    }
+                  }}>{"SUBTRACT"}</div><br/>
+                  <div className="cart_item_removeall" id={"cart_item" + i.itemnumber + "_removeall"} onClick={function() {
+                    i.quantity = 0;
                     document.getElementById('cart_item' + i.itemnumber).style.display = "none";
-                  }
-                }}>{"SUBTRACT"}</div><br/>
-                <div className="cart_item_removeall" id={"cart_item" + i.itemnumber + "_removeall"} onClick={function() {
-                  i.quantity = 0;
-                  document.getElementById('cart_item' + i.itemnumber).style.display = "none";
-                }}>REMOVE ALL</div>
+                  }}>REMOVE ALL</div>
 
 
+                </div>
               </div>
             </div>
           ))}
